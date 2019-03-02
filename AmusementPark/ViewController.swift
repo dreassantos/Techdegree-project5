45a471a739c2aef20a87543c85d129baa5b15218
Andrea Santos <dreasalazarsantos@gmail.com>
2019-03-02 10:11:29 -0800

    Initial Commit

---
+++ AmusementPark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmusementPark
+//
+//  Created by Andrea on 3/2/19.
+//  Copyright © 2019 SantosAndrea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
